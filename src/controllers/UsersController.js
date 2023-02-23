d11a7ba856af640fcbc8c2248afe2cc5e24996ba
douglas <38635101+dougdbp@users.noreply.github.com>
2023-02-23 11:32:13 -0300

    BugFix: database now running.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,16 @@
 const { hash, compare } = require("bcryptjs");
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
 
     async create(request, response) {
         const { name, email, password } = request.body;
-
+    
         const database = await sqliteConnection();
-        const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)" [email])
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-        if(checkUsersExists){
+        if(checkUserExists){
             throw new AppError("Este e-mail já está em uso.");
         }
 
